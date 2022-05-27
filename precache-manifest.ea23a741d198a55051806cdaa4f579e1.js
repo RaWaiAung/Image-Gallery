14a15bc4f7df17f5bfb684d12dcf807224787022
Ra Wai UCSM <yawaithaethae133@gmail.com>
2022-05-27 06:57:57 -0700

    Updates

---
+++ precache-manifest.ea23a741d198a55051806cdaa4f579e1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ba63d772f6763c77f12c552c99f889",
+    "url": "/Image-Gallery/index.html"
+  },
+  {
+    "revision": "fd662113747e80f2b875",
+    "url": "/Image-Gallery/static/css/main.e56a49e6.chunk.css"
+  },
+  {
+    "revision": "97136ec2aa4a187c38ab",
+    "url": "/Image-Gallery/static/js/2.9eec4e5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image-Gallery/static/js/2.9eec4e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd662113747e80f2b875",
+    "url": "/Image-Gallery/static/js/main.76dfe506.chunk.js"
+  },
+  {
+    "revision": "0f19380f19da9aba9345",
+    "url": "/Image-Gallery/static/js/runtime-main.277dd459.js"
+  },
+  {
+    "revision": "7106926c30bda651b5df134cb71a0757",
+    "url": "/Image-Gallery/static/media/pexels-binyamin-mellish-106399.7106926c.jpg"
+  },
+  {
+    "revision": "7f75b2f5221d44e8aad051afe6b1d44d",
+    "url": "/Image-Gallery/static/media/pexels-chevanon-photography-1108099.7f75b2f5.jpg"
+  },
+  {
+    "revision": "ea0eec73a6df51e1591a55f5049c6f50",
+    "url": "/Image-Gallery/static/media/pexels-gord-maclean-750697.ea0eec73.jpg"
+  },
+  {
+    "revision": "358663c8e2012aeef78f17f36f06d8c6",
+    "url": "/Image-Gallery/static/media/pexels-marc-onana-5823764.358663c8.jpg"
+  },
+  {
+    "revision": "622ba74c63731f37cd3bf3fb5bc6ceaf",
+    "url": "/Image-Gallery/static/media/pexels-mehmet-turgut-kirkgoz-11693940.622ba74c.jpg"
+  },
+  {
+    "revision": "1db626e5f03e872171cf03092e1592bb",
+    "url": "/Image-Gallery/static/media/pexels-pixabay-247431.1db626e5.jpg"
+  },
+  {
+    "revision": "a785fd9926a59e81a78d1177254dbbc2",
+    "url": "/Image-Gallery/static/media/pexels-pixabay-45853.a785fd99.jpg"
+  },
+  {
+    "revision": "9dde38a10ab3d432e2b4fb27fa88bf13",
+    "url": "/Image-Gallery/static/media/pexels-samer-daboul-2581153.9dde38a1.jpg"
+  },
+  {
+    "revision": "525c28ad35e70f980db0297ffdfba969",
+    "url": "/Image-Gallery/static/media/pexels-sevenstorm-juhaszimrus-402556.525c28ad.jpg"
+  },
+  {
+    "revision": "fe289da30e78517f39a81238d81eec3e",
+    "url": "/Image-Gallery/static/media/pexels-trygve-finkelsen-2893177.fe289da3.jpg"
+  },
+  {
+    "revision": "c1d2454e0b52973e307912ef7e558203",
+    "url": "/Image-Gallery/static/media/pexels-zhang-kaiyv-6702288.c1d2454e.jpg"
+  }
+]);
